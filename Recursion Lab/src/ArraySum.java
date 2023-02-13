2285359ea0e35d08e76b814e7fd0868ca1d9f015
sbearam <92243783+sbearam@users.noreply.github.com>
2023-02-12 21:22:20 -0500

    Add files via upload

---
+++ Recursion Lab/src/ArraySum.java
@@ -0,0 +1,13 @@
+public class ArraySum 
+{
+    public int sumOfArray(Integer[] a, int index)
+    {
+        if(index != 0)
+        {
+        	return a[index] + sumOfArray(a, --index);
+        }
+       return a[index];
+            
+        
+    }
+}
