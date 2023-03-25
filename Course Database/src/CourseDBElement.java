2961d3ff86a85cbb86f57af7a87b0f43dcb42faf
sbearam <92243783+sbearam@users.noreply.github.com>
2023-03-25 17:44:14 -0400

    Add files via upload

---
+++ Course Database/src/CourseDBElement.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class CourseDBElement implements Comparable
+{
+	String id, roomNum, instructor;
+	int crn, credits;
+	Comparator<T> c;
+	
+	CourseDBElement()
+	{
+		this.id = null;
+		this.roomNum = null;
+		this.instructor = null;
+		crn = 0;
+		credits = 0;
+	}
+}
